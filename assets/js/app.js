91bca05758a553ae8b0aff0c7f003684d0392a8b
suprru <supremeorru1@gmail.com>
2024-10-18 13:47:31 +0100

    🛒 created cartPage and data persistence

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: '#app',
+    data: {
+        courses: [
+            {id: 1, subject: 'biology 301', location: 'London', price: 80, space: 4},
+            {id: 2, subject: 'maths 101', location: 'Online', price: 50, space: 20},
+            {id: 3, subject: 'french', location: 'France', price: 90, space: 10},
+            {id: 4, subject: 'history', location: 'Egypt', price: 23, space: 9},
+            {id: 5, subject: 'CS50 2020', location: 'Kenya', price: 65, space: 5},
+            {id: 6, subject: 'humanities', location: 'Japan', price: 45, space: 7},
+        ],
+        cart: [5]
+    },
+    methods: {
+        addToCart(param){
+            this.cart.push(param);
+            localStorage.setItem('cart', JSON.stringify(this.cart));
+            alert(" added to cart!");   
+        },
+        checkout(param) {
+            console.log(param);
+        },
+        removeItemFromCart(param) {
+            this.cart = this.cart.filter(item => item.id !== param.id);
+            localStorage.setItem('cart', JSON.stringify(this.cart));
+        }
+    }
+})
