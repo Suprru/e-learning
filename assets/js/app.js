8268dcfcb3b92c5867b74012d56201548ee11499
suprru <supremeorru1@gmail.com>
2024-10-19 07:42:16 +0100

    ✅ finishing up cartPage

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,12 @@ new Vue({
     data: {
         courses: [
             {id: 1, subject: 'biology 301', location: 'London', price: 80, space: 4},
-            {id: 2, subject: 'maths 101', location: 'Online', price: 50, space: 20},
+            {id: 2, subject: 'maths 101', location: 'Online', price: 50, space: 30},
             {id: 3, subject: 'french', location: 'France', price: 90, space: 10},
             {id: 4, subject: 'history', location: 'Egypt', price: 23, space: 9},
             {id: 5, subject: 'CS50 2020', location: 'Kenya', price: 65, space: 5},
             {id: 6, subject: 'humanities', location: 'Japan', price: 45, space: 7},
+            {id: 7, subject: 'English', location: 'United Kingdom', price: 95, space: 0},
         ],
         cart: [],
     },
